e433cb24ba86d7d15e46b31d70cc2e80afab516f
eon <noe.navarro1019@gmail.com>
2022-03-23 23:46:26 -0700

    added gallery to the page to be rendered conditionally when a certain nav element is selected

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,33 +7,31 @@ import Nav from '..';
 afterEach(cleanup);
 
 describe('Nav component', () => {
-  // baseline test
   it('renders', () => {
     render(<Nav />);
   });
 
-  // snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-  
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
-// check for emoji within JSX
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
     const { getByLabelText } = render(<Nav />);
 
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})  
+})
 
-// check to ensure links are visible
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
